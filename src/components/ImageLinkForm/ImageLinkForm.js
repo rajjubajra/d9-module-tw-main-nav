e07fa651052f8f451b716f014b064c3134c3c950
rajju <vajrachara@yahoo.com>
2018-10-16 04:44:22 +0100

    Layout designed

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+  return(
+    <div>
+      <p className="f3">This app detect face in the picture. Try it pasting image link</p>
+      <div className="center pa4 shadow-2 br3 w-50">
+          <input className="w-70 fa4 pa2 center" type="text"  />
+          <button className="w-30 grow pa2 fa3 center">Detect</button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rank = () => {
+  return(
+    <div>
+      <p className="f4">Name your current rank is #5</p>
+    </div>
+  );
+}
+
+export default Rank;
