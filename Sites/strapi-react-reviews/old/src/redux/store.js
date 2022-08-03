efcddb62cc2b92bbaf09eaaf16b0595170a95f70
rajjubajra <rajjubajra@gmail.com>
2022-08-03 07:05:22 +0100

    updated database config

--- Sites/strapi-react-reviews/old/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import loginuserReducer from './loginSlice';
-
-
-export default configureStore({
-  reducer: {
-    login_user:loginuserReducer
-  }
-})
